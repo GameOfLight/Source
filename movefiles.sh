6a0803cdb443a281a2ee4b236e5fc8987858c713
Eivind Wikheim <eivinwi@student.matnat.uio.no>
2013-04-23 14:20:11 +0200

    Added bash-script for setting up libraries

--- movefiles.sh
+++ movefiles.sh
@@ -2,7 +2,7 @@
 
 DEST_DIR="/usr/share/arduino/libraries"
 
-echo "Script for copying GoL folders to <arduino-folder>/libraries"
+echo "Script for copying GoL folders to ${DEST_DIR}/libraries"
 echo "Call this script standing in GoL's source-folder"
 sudo cp -r GameOfLightSim ${DEST_DIR}
 sudo cp -r GameOfLightHW ${DEST_DIR}
