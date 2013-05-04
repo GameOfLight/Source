c7fd82f6c7e231fc35ccb7c200de6801c638dec8
Sigmund Hansen <sigmunha@ifi.uio.no>
2013-05-04 18:02:50 +0200

    Added missing closing parentheses to ternary expressions in getButtons.

--- GameOfLightSim/GameOfLightSim.cpp
+++ GameOfLightSim/GameOfLightSim.cpp
@@ -127,8 +127,8 @@ void GameOfLightSim::getButtons() {
 
 	if(bytes_red == NUM_PLAYERS*2) { 
 		for(int i = 0; i < NUM_PLAYERS; i+=2) {
-			Start[i] = ((serial_data[i] & (1 << 7) == 0)? 1 : 0;
-			Select[i] = ((serial_data[i] & (1 << 6) == 0)? 1 : 0;
+                        Start[i] = ((serial_data[i] & (1 << 7)) == 0)? 1 : 0;
+                        Select[i] = ((serial_data[i] & (1 << 6)) == 0)? 1 : 0;
 			L[i] = ((serial_data[i] & (1 << 5)) == 0)? 1 : 0;
 			R[i] = ((serial_data[i] & (1 << 4)) == 0)? 1 : 0; 
 			N[i] = ((serial_data[i+1] & (1 << 7)) == 0)? 1 : 0;
