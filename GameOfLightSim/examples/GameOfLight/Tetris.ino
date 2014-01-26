4edb9141f66b04b19e157c6370fc0013562c76fb
stiansel <leagle@online.no>
2014-01-26 12:40:34 +0100

    Added ghost piece and hard drop. Included a list of controls. Brought up to date with new goto/print behaviour.

--- GameOfLightSim/examples/GameOfLight/Tetris.ino
+++ GameOfLightSim/examples/GameOfLight/Tetris.ino
@@ -17,6 +17,25 @@
   You should have received a copy of the GNU General Public License
   along with Game Of Light.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+
+/*
+* Controls:
+* LEFT/RIGHT       - move left/right
+* UP               - drops the piece into the indicated position. (no lock delay)
+* DOWN             - increase gravity (lock delay)
+* Shoulder buttons - rotate the piece
+*
+* Max players 3, minimum 1.
+* Play areas are assigned in the following order: left, right, middle. Based on who pressed their startbutton first
+*
+* Score:
+* Clearing 1, 2, 3 or 4 rows gives 1, 3, 5 and 8 points respectivly. 
+* No bonus based on level or difficult rotations.
+* Points are tracked up to 65,535, but only the first 999 are displayed. +++ displayed for scores above this.
+*
+* The game speeds up automatically for every 10 rows cleared. Max level 20.
+*/
 #include <GameOfLightSim.h>
 #include <avr/pgmspace.h>
 
@@ -25,7 +44,6 @@ extern int8_t player[4]; //must call menu_playerStart for valid content
 extern uint8_t playerCnt; //must call menu_playerStart for valid count
 extern void menu_playerStart(uint8_t minPlayers, int8_t press_start_line, const int8_t player_x[4], const int8_t player_line[4]);
 extern uint8_t rand_8(); //from Snake.ino
-extern int freeRam();
 
 #define TETRIS_NORMAL 0
 #define TETRIS_DEAD 1
@@ -37,9 +55,13 @@ extern int freeRam();
 
 #define TETRIS_SPAWNHEIGHT 18
 
+//TODO:
+//Fix bug occuring when the game board is full. A new piece is redrawn on top of old content
+
 
 struct piece {
   uint8_t x, y;
+  uint8_t ghost_x, ghost_y; //Where did we last draw the indicator of where this piece will land?
   uint8_t type;
   uint8_t rotation;
 };
@@ -66,7 +88,7 @@ uint8_t tetris_boundsCheckDisable; //Set to 1 to allow drawing of tetrispieces o
 void tetris_splash() {
   frame.clear();
 
-  frame.gotoXY(14, 3);
+  frame.gotoXY(14, 24);
   frame.print("TETRIS", RED);
 
   menu_flipArea(3, 32, 4, 0b01); //Turn the R into a cyrillic Ya. A missuse of letters and tribute to the original game :)
@@ -163,6 +185,8 @@ void tetris_getNextPiece(uint8_t p, uint8_t pieces_next[3], struct piece* active
   active->rotation = 0;
   active->x = posLookup[p];
   active->y = TETRIS_SPAWNHEIGHT;
+  active->ghost_x = 0; //Since this isn't equal to active->x, will force a recalculation of ghost
+  active->ghost_y = 0; //Set <22 so that we don't clear something as soon as the next piece starts
 
   //Gets and displays the next piece this player will get as well as returning the new current piece
   pieces_next[p] = tetris_randomPiece(p);
@@ -194,7 +218,7 @@ void tetris_clearPiece(uint8_t x, uint8_t y, uint8_t piece, uint8_t rotation) {
 
 
 
-uint8_t tetris_drawPiece(uint8_t x, uint8_t y, uint8_t piece, uint8_t rotation) {
+void tetris_drawPiece(uint8_t x, uint8_t y, uint8_t piece, uint8_t rotation) {
   int8_t xmod, ymod;
   uint8_t colour;
   colour = pgm_read_byte(&tetris_colours[piece]);
@@ -211,8 +235,41 @@ uint8_t tetris_drawPiece(uint8_t x, uint8_t y, uint8_t piece, uint8_t rotation) 
       frame.setPixel(x + xmod + 1, y + ymod + 1, colour);
     }
   }
+}
 
-  return 0;
+
+void tetris_drawGhostPiece(uint8_t p, struct piece* active, uint8_t hasRotated) {
+  if ((active->x != active->ghost_x) || hasRotated) {
+    //The ghost only moves if the player has shifted left or right or if a rotation has been executed
+    uint8_t offset = 2;
+    while (tetris_checkLocation(p, active->type, active->x, active->y + offset, active->rotation) == 0) {
+      //Move one square down and repeat until we hit something
+      offset += 2;
+    }
+    active->ghost_y = active->y + offset - 2;
+    active->ghost_x = active->x;
+  }
+
+  if (active->ghost_y != active->y) {
+    int8_t xmod, ymod;
+    uint8_t colour = pgm_read_byte(&tetris_colours[active->type]);
+    //Redraw the dotted outline of where this piece will land from it's current position
+    for (int px = 0; px < 4; px++) {
+      xmod = pgm_read_byte(&tetris_pieces[active->type][active->rotation][px][0]);
+      ymod = pgm_read_byte(&tetris_pieces[active->type][active->rotation][px][1]);
+      if (active->ghost_y+ymod >= 22) {
+        //Only half the pixels are filled in allowing the user to discern the ghost from the real piece
+        frame.setPixel(active->ghost_x + xmod, active->ghost_y + ymod, colour);
+        frame.setPixel(active->ghost_x + xmod + 1, active->ghost_y + ymod + 1, colour);
+      }
+    }
+  }
+}
+
+
+void tetris_clearGhostPiece(struct piece* active) {
+  //Clear the ghost outline showing where this piece will end up if it fell straight down
+  tetris_clearPiece(active->ghost_x, active->ghost_y, active->type, active->rotation);
 }
 
 
@@ -256,6 +313,7 @@ uint8_t tetris_move(uint8_t p, struct piece* active, uint8_t dir) {
   return 0; //No (legal) move executed
 }
 
+
 uint8_t tetris_kickPiece(uint8_t p, struct piece* active) {
   //If current location is illegal, will check to the right then left.
   // This allows rotation when near a wall
@@ -272,6 +330,7 @@ uint8_t tetris_kickPiece(uint8_t p, struct piece* active) {
 
   return 0; //Illegal rotation, can't easily kick into a legal one either
 }
+
 
 uint8_t tetris_rotatePiece(uint8_t p, struct piece* active) {
   uint8_t oldRotation = active->rotation;
@@ -326,9 +385,9 @@ uint8_t tetris_lineClearedCheck(uint8_t p, struct piece* active) {
 }
 
 
-uint8_t tetris_isWithinSpawnBox(uint8_t p, struct piece* active) {
+uint8_t tetris_isWithinSpawnBox(struct piece* active) {
   //Checks if any part of the locked piece is above the play area
-  // returns 0 if that is the case.
+  // returns 1 if that is the case.
   int8_t ymod;
 
   for (int px = 0; px < 4; px++) {
@@ -353,12 +412,8 @@ void tetris_drawBoard() {
 
   //Draw dotted top and bottom lines:
   for (int i = 1; i < 64; i+=2) {
-//    if (i & 1) {
       frame.setPixel(i, 21, ORANGE);
       frame.setPixel(i, 62, ORANGE);
-  /*  } else {
-      frame.setPixel(i, 63, ORANGE);
-    }*/
   }
 
   //Draw vertical dotted dividing lines:
@@ -370,6 +425,7 @@ void tetris_drawBoard() {
   }
   frame.update();
 }
+
 
 void tetris_drawLevelLine(uint8_t p, uint8_t progress) {
   //Draw the little line that tells the player how far it is to the next level
@@ -383,7 +439,7 @@ void tetris_drawLevelLine(uint8_t p, uint8_t progress) {
 
 void tetris_drawScore(uint8_t p, uint16_t score) {
   //Score has apparently changed. Update the display!
-  frame.gotoXY(tetris_bounds[p][0]+2, 1);
+  frame.gotoXY(tetris_bounds[p][0]+2, 8);
   frame.clear(17);
   if (score <= 999) {
     char buff[4];
@@ -401,7 +457,7 @@ void tetris_drawScore(uint8_t p, uint16_t score) {
 
 void tetris_newGame() {
   const int8_t x_coord[] = {5, 47, 26, -1};
-  const int8_t y_coord[] = {5, 5, 5, -1};
+  const int8_t y_coord[] = {40, 40, 40, -1};
   //Reset used pieces
   tetris_pieceBag[0] = 0;
   tetris_pieceBag[1] = 0;
@@ -413,11 +469,8 @@ void tetris_newGame() {
   //Remove the "Tetris" print so that we may display the "press/start" print on that line:
   frame.gotoXY(14, 0);
   frame.clear(35);
-  Serial.println("fetch players");
   menu_playerStart(1, 0, x_coord, y_coord); //Fetch players
   tetris_drawBoard(); //Lost our Tetris title when we fetched the players. Redraw it.
-
-  Serial.println("return");
 }
 
 
@@ -433,9 +486,10 @@ void tetris() {
 
   for (int i = 0; i < playerCnt; i++) {
     //generate the first pieces and display them in the next piece area
+    pieces_next[i] = 0; //Has to be set to something or else we might clear an unintended part of the screen first time
     tetris_getNextPiece(i, pieces_next, &pieces[i]);
     last_time[i] = 0;
-    level[i] = 0;
+    level[i] = 140; //For testing purposes. Set to 0 later
     scores[i] = 0;
     player_state[i] = TETRIS_NORMAL;
     rowsAtOnce[i] = 0;
@@ -450,7 +504,6 @@ void tetris() {
   }
   anyAlive = 1;
 
-
   //Main game loop
   do {
     uint8_t updated = 0;
@@ -462,7 +515,7 @@ void tetris() {
         last_time[i] = currTime;
         updated = 1;
         if (player_state[i] == TETRIS_LOCKED) {
-          //Current piece locked in place. Check and clear any applicable lines
+          //PIECE PLACED. CHECK IF IT MADE ANY LINES FULL
 
           player_state[i] = tetris_lineClearedCheck(i, &pieces[i]);
           //player_state[i] now contains a line number or is 0 if no clearable lines were found
@@ -503,8 +556,9 @@ void tetris() {
           }
 
         } else if (player_state[i] >= TETRIS_CLEARLINE) {
-          //Line cleared. Coordinate in player_state[i]. Move anything above that down
-         
+          //REMOVAL OF FULL LINES
+
+          //Line cleared. Coordinate in player_state[i]. Move anything above that down         
           //THE SLOOOOOW WAY OF DOING IT. Swap with a bytebased method later!
           for (uint8_t y = player_state[i]; y >= 24; y-=2) { //Each line
             for (uint8_t x = tetris_bounds[i][0]; x < tetris_bounds[i][1]-1; x+=2) {
@@ -522,24 +576,43 @@ void tetris() {
           player_state[i] = TETRIS_LOCKED;
 
         } else {
+          //PLAYER INTERACTION
+
           uint8_t rotateSuccess, dir;
 
-          //Clear current position:
+          //Clear current position and it's drawn ghost outline:
           tetris_clearPiece(pieces[i].x, pieces[i].y, pieces[i].type, pieces[i].rotation);
+          tetris_clearGhostPiece(&pieces[i]);
 
           //Translate piece left/right
           dir = frame.getDir(player[i]);//internal player number to controller number and get it's direction
-          tetris_move(i, &pieces[i], dir);
-          
-          //Rotate piece if asked. Add left/right wall kick if needed to avoid the outer walls
-          rotateSuccess = tetris_rotatePiece(i, &pieces[i]);
+          if (dir == NORTH) {
+            //Hard drop!
+            if (pieces[i].ghost_x != 0) {
+              //Only do something if the ghost image has been calculated
+              //This reduces accidental double-activation due to holding the key a smidge too long.
 
-          if (player_state[i] == TETRIS_GRACEPERIOD_IMMUNE) {
-            player_state[i] = TETRIS_NORMAL;
-          } else if (rotateSuccess) {
-            player_state[i] = TETRIS_GRACEPERIOD;
+              //Hard drop => Warp piece to ghost location
+              pieces[i].x = pieces[i].ghost_x;
+              pieces[i].y = pieces[i].ghost_y;
+              //Skip lock delay and jump straight to gravity locking the piece in place:
+              player_state[i] = TETRIS_LOCK_DELAY;
+            }
+
+          } else {
+            //Translate piece
+            tetris_move(i, &pieces[i], dir);
+            
+            //Rotate piece if asked. Add left/right wall kick if needed to avoid the outer walls
+            rotateSuccess = tetris_rotatePiece(i, &pieces[i]);
+
+            if (player_state[i] == TETRIS_GRACEPERIOD_IMMUNE) {
+              player_state[i] = TETRIS_NORMAL;
+            } else if (rotateSuccess) {
+              player_state[i] = TETRIS_GRACEPERIOD;
+            }
           }
-          //Translate piece
+          
           if (player_state[i] == TETRIS_NORMAL || player_state[i] == TETRIS_LOCK_DELAY) {
             //Gravity
             if (tetris_move(i, &pieces[i], SOUTH)) {
@@ -549,7 +622,7 @@ void tetris() {
               //Piece blocked from falling
               if (player_state[i] == TETRIS_LOCK_DELAY) {
                 //Unable to move for two frames, lock the piece!
-                if (tetris_isWithinSpawnBox(i, &pieces[i])) {
+                if (tetris_isWithinSpawnBox(&pieces[i])) {
                   //Piece locked within spawnbox. Game over!
                   player_state[i] = TETRIS_DEAD;
                   anyAlive = 0;
@@ -573,7 +646,9 @@ void tetris() {
             //Reset grace period
             player_state[i] = TETRIS_GRACEPERIOD_IMMUNE;
           }
-          //Redraw piece
+
+          //Redraw ghost outline and the actual piece:
+          tetris_drawGhostPiece(i, &pieces[i], rotateSuccess);
           tetris_drawPiece(pieces[i].x, pieces[i].y, pieces[i].type, pieces[i].rotation);
 
 /*          Serial.print("Player ");
